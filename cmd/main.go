82d097ad401ce79694d4eb2eb32292c41312f8ce
Hashim N <hashimn@Hashims-MacBook-Air.local>
2025-12-12 21:53:05 +0530

    bugs fixed

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,31 @@
-package main
+	package main
 
-import (
-	"MechOpss/infra/db"
-	"MechOpss/internal/src/controllers"
-	"MechOpss/internal/src/repository"
-	"MechOpss/internal/src/routes"
-	"MechOpss/internal/src/services"
-	"github.com/gin-gonic/gin"
-)
+	import (
+		"MechOpss/infra/db"
+		"MechOpss/internal/src/controllers"
+		"MechOpss/internal/src/repository"
+		"MechOpss/internal/src/routes"
+		"MechOpss/internal/src/services"
+		"github.com/gin-gonic/gin"
+	)
 
-func main() {
-	r := gin.Default()
-	//1
-	DB := db.Connection()
-	//2
-	UserRepo := repository.Newrepo(DB)
-	//3
-	userService := services.NewUserservices(UserRepo)
-	//4
-	userController := controllers.NewController(userService)
-	//5
-	routes.UserRoutes(r, userController)
-	r.LoadHTMLGlob("../templates/*.html")
-	routes.StaffRoutes(r)
-	routes.AdminRoute(r)
+	func main() {
+		r := gin.Default()
+		//1
+		DB := db.Connection()
+		//2
+		UserRepo := repository.Newrepo(DB)
+		//3
+		userService := services.NewUserservices(UserRepo)
+		//4
+		userController := controllers.NewController(userService)
+		//5
+		routes.UserRoutes(r, userController)
+		r.LoadHTMLGlob("../templates/*.html")
 
-	r.Run(":8008")
-}
+		routes.StaffRoutes(r)
+
+		routes.AdminRoute(r)
+
+		r.Run(":8008")
+	}
